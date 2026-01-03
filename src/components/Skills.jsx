9adda62f04203ccb2cce531ac5b40b01d11f1ead
pariarul <pariaarul@gmail.com>
2026-01-04 04:31:27 +0530

    skill java and springboot add

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,14 +25,18 @@ import { VscJson } from 'react-icons/vsc';
    SKILLS DATA
 ======================= */
 const skills = [
-  { name: 'HTML5', icon: <FaHtml5 />, color: '#E34F26' },
-  { name: 'CSS3', icon: <FaCss3Alt />, color: '#1572B6' },
+ 
   { name: 'JavaScript', icon: <FaJs />, color: '#F7DF1E' },
   { name: 'React', icon: <FaReact />, color: '#61DAFB' },
   { name: 'Next.js', icon: <SiNextdotjs />, color: '#ffffff' },
 
+   { name: 'HTML5', icon: <FaHtml5 />, color: '#E34F26' },
+  { name: 'CSS3', icon: <FaCss3Alt />, color: '#1572B6' },
+
   { name: 'Node.js', icon: <FaNodeJs />, color: '#339933' },
   { name: 'Express.js', icon: <SiExpress />, color: '#ffffff' },
+  { name: 'Java', icon: <FaJava />, color: '#007396' },
+  {name: 'Spring Boot', icon: <SiSpringboot />, color: '#6DB33F'},
   { name: 'MongoDB', icon: <SiMongodb />, color: '#47A248' },
   { name: 'MySQL', icon: <SiMysql />, color: '#4479A1' },
 
